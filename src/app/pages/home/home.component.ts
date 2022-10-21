e131c3d36b059182f37bdf47ef92d8b5f9017e39
Suthasinee <81916732+SuthasineePhasiw@users.noreply.github.com>
2022-10-21 19:30:36 +0700

    third commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  head = 'Hello World';
+  name = 'Sutasinee Phasiw';
+  position = 'Fornt-end Developer';
+  description = 'Hello, I am Immage. I useed to be an intern as an HTML Editor. I have a good understanding of HTML CSS SCSS. Now, I am leaning how to use Angular and I am very responsible and able to socialize.';
+
   constructor() { }
 
   ngOnInit(): void {
